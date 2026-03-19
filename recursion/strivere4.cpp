cc8d299c9c93f898fa478b1264923bcb2cb5af53
aditya0944 <adityarajshukla7566@gmail.com>
2026-03-19 22:59:36 +0530

    coding

---
+++ recursion/strivere4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;              // input a character
+
+    cout << int(a[0]);        // convert to ASCII and print
+    return 0;
+}
